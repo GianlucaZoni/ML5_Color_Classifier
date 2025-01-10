f5a38b14c33b0a4633c753a182107a315a08ecd2
GianY <gianlucazoni@outlook.com>
2025-01-10 10:41:49 +0100

    testing early stopping

--- sketch.js
+++ sketch.js
@@ -144,8 +144,8 @@ function setup() {
 
   // Step 6: train your neural network
   const trainingOptions = {
-    epochs: 2048,
-    batchSize: 64,
+    epochs: 256,
+    batchSize: 32,
   };
   classifier.train(trainingOptions, finishedTraining);
 }
